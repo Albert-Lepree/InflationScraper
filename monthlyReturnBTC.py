5b0b9fd0d6d9fc1e0c56bee8f5192216892f60e6
finn <finn244@hotmail.com>
2021-11-04 09:23:37 -0400

    added comments and a method for finding month of highest return

--- monthlyReturnBTC.py
+++ monthlyReturnBTC.py
@@ -3,6 +3,18 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def merge_and_plot_data():
+    pd.set_option("display.max_rows", 202, "display.max_columns", None) #shows all columns in the dataframes
+
+    df = pd.DataFrame(monthly_btc_crawler(), columns=['month', '%return']) #creates data frame
+    df2 = pd.DataFrame(percent_change_DXY(), columns=['month', 'DXY%return'])
+
+    result = pd.merge(df, df2, on='month') #merges the dataframes
+    print(result)
+
+    result.plot(x='month', y=['%return', 'DXY%return'], kind='line') #plots the data
+    plt.show()
+
 def monthly_btc_crawler():
     pd.set_option("display.max_rows", 202, "display.max_columns", None)  # shows all columns in the dataframes
 
@@ -61,14 +73,58 @@ def monthly_btc_crawler():
             month.append(array[i])
         else:
             array[i] = array[i].rstrip((array[i])[-1])  # removes % sign to be converted to float
-            print(array[i])
-            print(i)
             percent.append(float(array[i]))  # adds ROI to list and converts to float
+
+    mostAlgo(month, percent)
 
     data = {'month' : month, "%return" : percent} # puts data into an array? to be put into data frame
     df = pd.DataFrame(data, columns=['month', '%return']) #creates dataframe
-    print(df)
-    df.plot(x='month', y='%return', kind='line') #plots the data
-    plt.show()
+#    print(df)
+#   df.plot(x='month', y='%return', kind='line') #plots the data
+#    plt.show()
 
-monthly_btc_crawler()
+    return data
+
+# monthly_btc_crawler()
+
+def percent_change_DXY():
+
+    DXY_df = pd.read_csv('./percentChangeDXY.csv')
+
+    # Convert the date data to datetime
+    dates = pd.Series(DXY_df['DATE'])
+    dates = pd.to_datetime(dates)
+
+    # Getting month name from month
+    # Need to rename this variable
+    DXY_df['MONTH'] = dates.dt.month_name(locale = 'English')
+
+    # Getting Year
+    DXY_df['Year'] = DXY_df['DATE'].str[0:4]
+
+    # New column with month and year
+    DXY_df['month'] = DXY_df['MONTH'] + " " + DXY_df['Year']
+
+    # Plot Date vs % Return
+#    print(DXY_df.columns)
+#    DXY_df.plot(x='month', y='DXY%return', kind='line')
+#    plt.xticks(rotation=90)
+#    plt.show()
+
+    data = {'month': DXY_df['month'], "DXY%return": DXY_df['DXY%return']}
+
+    return data
+
+## prints the highest return month and percent
+def mostAlgo(time, percent):
+    most = 0.0
+    mostIndex = 0
+
+    for i in range(len(percent)-1): # loops through array comparing each index
+        if percent[i] > most:
+            most = percent[i]
+            mostIndex = i # saves index to select month to be printed
+
+    print(f'The Highest return was {most}% during the month of {time[mostIndex]}.')
+
+merge_and_plot_data()
