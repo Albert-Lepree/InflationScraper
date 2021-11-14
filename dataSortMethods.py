f0fe6b7ae6a9ff63c0bf792aada91b0ec27b7881
AlbertLepree <aflepree4765@eagle.fgcu.edu>
2021-11-14 12:50:11 -0500

    added dataSortAlgos

--- dataSortMethods.py
+++ dataSortMethods.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 ## prints the highest return month and percent
 def mostAlgo(time, percent):
     most = 0.0
@@ -32,3 +34,26 @@ def similarityAlgo(btc, spx):
     print(f'Bitcoin was negative while the S&P was positive {BTCdownSPXup} times since inception')
     print(f'Bitcoin was positive while the S&P was positive {BTCupSPXup} times since inception')
     print(f'Bitcoin was negative while the S&P was negative {BTCdownSPXdown} times since inception')
+
+def least_algo(time, percent):
+    least = 0.0
+    least_index = 0
+
+    for i in range(len(percent)-1): # loops through array comparing each index
+        if percent[i] < least:
+            least = percent[i]
+            least_index = i # saves index to select month to be printed
+
+    print(f'The Highest return was {least}% during the month of {time[least_index]}.')
+
+# Returns months that performed better than 5%
+def positive_performance(month, percent):
+
+    count = Counter()
+
+    for i in range(len(percent)):
+        if percent[i] > 5:
+            count[month[i]] += 1
+    print(f'Months with a return above 5%: \n {count}')
+
+positive_performance(['Oct', 'Dec', 'Oct', 'Feb', 'March'], [6, 3, 6, 10, 10])
